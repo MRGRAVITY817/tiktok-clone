7e4570b276ad46d081d87742bdc8b684d4f444c3
Hoon Wee <mrgravity817@gmail.com>
2023-03-14 07:39:20 +0900

    feat: add login screen

--- lib/features/authentication/login_screen.dart
+++ lib/features/authentication/login_screen.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:ticktok_clone/constants/gaps.dart';
+import 'package:ticktok_clone/constants/sizes.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
+  void onSignUpTap(BuildContext context) {
+    // Login screen is stacked above sign-up screen,
+    // So we should pop this screen to go back to sign-up screen
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
+              child: Column(
+                children: const [
+                  Gaps.v80,
+                  Text("Log in to TikTok",
+                      style: TextStyle(
+                          fontSize: Sizes.size24, fontWeight: FontWeight.w700)),
+                  Gaps.v20,
+                  Text(
+                    "Manage your account, check notifications, comment on videos, and more.",
+                    style: TextStyle(
+                        fontSize: Sizes.size16, color: Colors.black45),
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ))),
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.grey.shade100,
+          elevation: 2,
+          child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: Sizes.size32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account?",
+                      style: TextStyle(fontSize: Sizes.size16)),
+                  Gaps.h5,
+                  GestureDetector(
+                      onTap: () => onSignUpTap(context),
+                      child: Text("Sign up",
+                          style: TextStyle(
+                              fontSize: Sizes.size16,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context).primaryColor)))
+                ],
+              ))),
+    );
   }
 }
