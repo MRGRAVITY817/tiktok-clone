e71c9ee7bf296e7eab4d2bb1f2616156863db89c
Hoon Wee <mrgravity817@gmail.com>
2023-03-14 09:09:16 +0900

    feat: add email screen and set global app bar theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticktok_clone/constants/sizes.dart';
 import 'package:ticktok_clone/features/authentication/sign_up_screen.dart';
 
 void main() {
@@ -13,8 +14,16 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
         title: 'TikTok Clone',
         theme: ThemeData(
-          primaryColor: const Color(0xFFE9435A),
-        ),
+            scaffoldBackgroundColor: Colors.white,
+            primaryColor: const Color(0xFFE9435A),
+            appBarTheme: const AppBarTheme(
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.white,
+                elevation: 0,
+                titleTextStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: Sizes.size16 + Sizes.size2,
+                    fontWeight: FontWeight.w600))),
         home: const SignUpScreen());
   }
 }
