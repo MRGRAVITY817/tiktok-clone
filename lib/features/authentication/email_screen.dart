fdd0adda12cfd646c5bf3b483b2b90d47f066838
Hoon Wee <mrgravity817@gmail.com>
2023-03-16 21:21:44 +0900

    feat: validate email input

--- lib/features/authentication/email_screen.dart
+++ lib/features/authentication/email_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ticktok_clone/constants/gaps.dart';
 import 'package:ticktok_clone/constants/sizes.dart';
+import 'package:ticktok_clone/features/authentication/password_screen.dart';
 import 'package:ticktok_clone/features/authentication/widgets/form_button.dart';
 
 class EmailScreen extends StatefulWidget {
@@ -36,40 +37,71 @@ class _EmailScreenState extends State<EmailScreen> {
     super.dispose();
   }
 
+  String? _isEmailValid() {
+    if (_email.isEmpty) return null;
+    // perform some validation
+    final regExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!regExp.hasMatch(_email)) {
+      return "Email not valid.";
+    }
+    return null;
+  }
+
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onSubmit() {
+    if (_email.isEmpty || _isEmailValid() != null) return;
+
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => const PasswordScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Sign up")),
-      body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
-          child: Column(
-            // Cross axis of column is row direction.
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Gaps.v40,
-              const Text("What is your email?",
-                  style: TextStyle(
-                      fontSize: Sizes.size24, fontWeight: FontWeight.w700)),
-              Gaps.v16,
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  hintText: "Email",
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey.shade400),
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(title: const Text("Sign up")),
+        body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+            child: Column(
+              // Cross axis of column is row direction.
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Gaps.v40,
+                const Text("What is your email?",
+                    style: TextStyle(
+                        fontSize: Sizes.size24, fontWeight: FontWeight.w700)),
+                Gaps.v16,
+                TextField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  onEditingComplete: _onSubmit,
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    errorText: _isEmailValid(),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey.shade400),
+                  cursorColor: Theme.of(context).primaryColor,
+                ),
+                Gaps.v28,
+                GestureDetector(
+                  onTap: _onSubmit,
+                  child: FormButton(
+                    disabled: _email.isEmpty || _isEmailValid() != null,
                   ),
                 ),
-                cursorColor: Theme.of(context).primaryColor,
-              ),
-              Gaps.v28,
-              FormButton(
-                disabled: _email.isEmpty,
-              ),
-            ],
-          )),
+              ],
+            )),
+      ),
     );
   }
 }
