68606a0a1e59e6ec2ccbac34841a9fc0c99b2fd9
Hoon Wee <mrgravity817@gmail.com>
2023-03-16 17:31:25 +0900

    feat: make auth button accept callback function

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ticktok_clone/constants/gaps.dart';
 import 'package:ticktok_clone/constants/sizes.dart';
+import 'package:ticktok_clone/features/authentication/email_screen.dart';
 import 'package:ticktok_clone/features/authentication/login_screen.dart';
 import 'package:ticktok_clone/features/authentication/widgets/auth_button.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
-  void onLoginTap(BuildContext context) {
+  void _onLoginTap(BuildContext context) {
     Navigator.of(context)
         .push(MaterialPageRoute(builder: (context) => const LoginScreen()));
+  }
+
+  void _onEmailTap(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const EmailScreen()));
   }
 
   @override
@@ -21,32 +27,34 @@ class SignUpScreen extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: const [
+          children: [
             Gaps.v80,
-            Text(
+            const Text(
               "Sign up for TikTok",
               style: TextStyle(
                   fontSize: Sizes.size24, fontWeight: FontWeight.w700),
             ),
             Gaps.v20,
-            Text(
+            const Text(
               "Create a profile, follow other accounts, make your own videos, and more.",
               style: TextStyle(fontSize: Sizes.size16, color: Colors.black45),
               textAlign: TextAlign.center,
             ),
             Gaps.v40,
             AuthButton(
-                text: "Use email & password",
-                icon: FaIcon(FontAwesomeIcons.user)),
+              text: "Use email & password",
+              icon: const FaIcon(FontAwesomeIcons.user),
+              onTap: () => _onEmailTap(context),
+            ),
             Gaps.v16,
-            AuthButton(
+            const AuthButton(
                 text: "Continue with Apple",
                 icon: FaIcon(FontAwesomeIcons.apple)),
           ],
         ),
       )),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.grey.shade100,
+        color: Colors.grey.shade50,
         elevation: 2,
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: Sizes.size32),
@@ -55,7 +63,7 @@ class SignUpScreen extends StatelessWidget {
                 style: TextStyle(fontSize: Sizes.size16)),
             Gaps.h5,
             GestureDetector(
-                onTap: () => onLoginTap(context),
+                onTap: () => _onLoginTap(context),
                 child: Text('Log in',
                     style: TextStyle(
                         fontSize: Sizes.size16,
--- lib/features/authentication/widgets/auth_button.dart
+++ lib/features/authentication/widgets/auth_button.dart
@@ -5,32 +5,37 @@ import 'package:ticktok_clone/constants/sizes.dart';
 class AuthButton extends StatelessWidget {
   final String text;
   final FaIcon icon;
+  final Function? onTap;
 
-  const AuthButton({super.key, required this.text, required this.icon});
+  const AuthButton(
+      {super.key, required this.text, required this.icon, this.onTap});
 
   @override
   Widget build(BuildContext context) {
     // `FractionallySizedBox` will expand horizontally til parent's width.
-    return FractionallySizedBox(
-      widthFactor: 1,
-      child: Container(
-        padding: const EdgeInsets.all(Sizes.size14),
-        decoration: BoxDecoration(
-            border:
-                Border.all(color: Colors.grey.shade300, width: Sizes.size1)),
-        // `Stack` literally stacks the child elements from device towards user.
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            // Icon should be placed on the left.
-            Align(alignment: Alignment.centerLeft, child: icon),
-            Text(
-              text,
-              style: const TextStyle(
-                  fontSize: Sizes.size16, fontWeight: FontWeight.w600),
-              textAlign: TextAlign.center,
-            )
-          ],
+    return GestureDetector(
+      onTap: () => onTap!(),
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        child: Container(
+          padding: const EdgeInsets.all(Sizes.size14),
+          decoration: BoxDecoration(
+              border:
+                  Border.all(color: Colors.grey.shade300, width: Sizes.size1)),
+          // `Stack` literally stacks the child elements from device towards user.
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              // Icon should be placed on the left.
+              Align(alignment: Alignment.centerLeft, child: icon),
+              Text(
+                text,
+                style: const TextStyle(
+                    fontSize: Sizes.size16, fontWeight: FontWeight.w600),
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
         ),
       ),
     );
