b1db2a41be5da952e614d2391a911dd9b304e33f
Hoon Wee <mrgravity817@gmail.com>
2023-03-18 17:59:39 +0900

    feat: route interest screen to tutorial screen

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ticktok_clone/constants/gaps.dart';
 import 'package:ticktok_clone/constants/sizes.dart';
+import 'package:ticktok_clone/features/onboarding/tutorial_screen.dart';
 import 'package:ticktok_clone/features/onboarding/widgets/interest_button.dart';
 
 class InterestsScreen extends StatefulWidget {
@@ -96,21 +97,34 @@ class _InterestsScreenState extends State<InterestsScreen> {
               left: Sizes.size24,
               right: Sizes.size24,
             ),
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                  vertical: Sizes.size16 + Sizes.size2),
-              decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-              child: const Text(
-                'Next',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                  fontSize: Sizes.size16,
+            child: GestureDetector(
+              onTap: _onNextTap,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                    vertical: Sizes.size16 + Sizes.size2),
+                decoration:
+                    BoxDecoration(color: Theme.of(context).primaryColor),
+                child: const Text(
+                  'Next',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: Sizes.size16,
+                  ),
                 ),
               ),
             ),
           )),
+    );
+  }
+
+  void _onNextTap() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const TutorialScreen(),
+      ),
     );
   }
 }
