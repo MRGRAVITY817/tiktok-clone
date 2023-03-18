e2a4c813b6bb67466188b94a7f14b5b9e66281bc
Hoon Wee <mrgravity817@gmail.com>
2023-03-18 17:44:14 +0900

    feat: interest button should be toggled

--- lib/features/onboarding/widgets/interest_button.dart
+++ lib/features/onboarding/widgets/interest_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ticktok_clone/constants/sizes.dart';
 
-class InterestButton extends StatelessWidget {
+class InterestButton extends StatefulWidget {
   const InterestButton({
     super.key,
     required this.interest,
@@ -10,30 +10,48 @@ class InterestButton extends StatelessWidget {
   final String interest;
 
   @override
+  State<InterestButton> createState() => _InterestButtonState();
+}
+
+class _InterestButtonState extends State<InterestButton> {
+  bool _isSelected = false;
+
+  void _onTap() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: Sizes.size16,
-        horizontal: Sizes.size24,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(Sizes.size32),
-        border: Border.all(
-          color: Colors.black.withOpacity(0.1),
+    return GestureDetector(
+      onTap: _onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 100),
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size16,
+          horizontal: Sizes.size24,
         ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            blurRadius: 5,
-            spreadRadius: 5,
+        decoration: BoxDecoration(
+          color: _isSelected ? Theme.of(context).primaryColor : Colors.white,
+          borderRadius: BorderRadius.circular(Sizes.size32),
+          border: Border.all(
+            color: Colors.black.withOpacity(0.1),
           ),
-        ],
-      ),
-      child: Text(
-        interest,
-        style: const TextStyle(
-          fontWeight: FontWeight.bold,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              blurRadius: 5,
+              spreadRadius: 5,
+            ),
+          ],
+        ),
+        child: Text(
+          widget.interest,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: _isSelected ? Colors.white : Colors.black87,
+          ),
         ),
       ),
     );
