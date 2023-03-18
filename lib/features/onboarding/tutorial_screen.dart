3c473826000c7a3b500ca30afc516cba85e06c27
Hoon Wee <mrgravity817@gmail.com>
2023-03-18 18:08:51 +0900

    feat: add default tab controller

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -1,10 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:ticktok_clone/constants/gaps.dart';
+import 'package:ticktok_clone/constants/sizes.dart';
 
-class TutorialScreen extends StatelessWidget {
+class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
 
   @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return DefaultTabController(
+      length: 3, // How many tab screens will we have
+      child: Scaffold(
+        body: SafeArea(
+          child: TabBarView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.size24,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Gaps.v52,
+                    Text(
+                      "Watch cool videos",
+                      style: TextStyle(
+                        fontSize: Sizes.size40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v16,
+                    Text(
+                      "Videos are personalized for you based on what you watch, like, and share.",
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.size24,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Gaps.v52,
+                    Text(
+                      "Tab 2",
+                      style: TextStyle(
+                        fontSize: Sizes.size40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v16,
+                    Text(
+                      "Videos are personalized for you based on what you watch, like, and share.",
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.size24,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Gaps.v52,
+                    Text(
+                      "Tab 3",
+                      style: TextStyle(
+                        fontSize: Sizes.size40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v16,
+                    Text(
+                      "Videos are personalized for you based on what you watch, like, and share.",
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
