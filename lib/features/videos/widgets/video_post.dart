c48020a0d01d3757bebf2a298d45b9f6012310db
Hoon Wee <mrgravity817@gmail.com>
2023-09-08 16:16:48 +0900

    feat: add animation controller for resizing play button when tapped

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -18,17 +18,29 @@ class VideoPost extends StatefulWidget {
   State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VideoPostState extends State<VideoPost> {
+class _VideoPostState extends State<VideoPost> with SingleTickerProviderStateMixin {
+  // Controllers
   final VideoPlayerController videoPlayerController =
       VideoPlayerController.asset("assets/videos/baby_video.mp4");
+  late final AnimationController animationController;
 
+  // States
   bool isPlaying = false;
-  Duration animationDuration = const Duration(milliseconds: 130);
+  Duration animationDuration = const Duration(milliseconds: 200);
 
   @override
   void initState() {
     super.initState();
+    // init controllers
     initVideoPlayer();
+    initAnimation();
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    animationController.dispose();
+    super.dispose();
   }
 
   void initVideoPlayer() async {
@@ -39,18 +51,22 @@ class _VideoPostState extends State<VideoPost> {
     videoPlayerController.addListener(onVideoChange);
   }
 
+  void initAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: animationDuration,
+      lowerBound: 1.0,
+      upperBound: 1.5,
+      value: 1.5,
+    );
+  }
+
   void onVideoChange() {
     if (videoPlayerController.value.isInitialized) {
       if (videoPlayerController.value.duration == videoPlayerController.value.position) {
         widget.onVideoFinished();
       }
     }
-  }
-
-  @override
-  void dispose() {
-    videoPlayerController.dispose();
-    super.dispose();
   }
 
   void onVisibilityChanged(VisibilityInfo info) {
@@ -75,13 +91,22 @@ class _VideoPostState extends State<VideoPost> {
           Positioned.fill(
             child: IgnorePointer(
               child: Center(
-                child: AnimatedOpacity(
-                  opacity: isPlaying ? 0 : 1,
-                  duration: animationDuration,
-                  child: const FaIcon(
-                    FontAwesomeIcons.play,
-                    color: Colors.white,
-                    size: Sizes.size52,
+                child: AnimatedBuilder(
+                  animation: animationController,
+                  builder: ((context, child) {
+                    return Transform.scale(
+                      scale: animationController.value,
+                      child: child,
+                    );
+                  }),
+                  child: AnimatedOpacity(
+                    opacity: isPlaying ? 0 : 1,
+                    duration: animationDuration,
+                    child: const FaIcon(
+                      FontAwesomeIcons.play,
+                      color: Colors.white,
+                      size: Sizes.size52,
+                    ),
                   ),
                 ),
               ),
@@ -95,8 +120,10 @@ class _VideoPostState extends State<VideoPost> {
   void onTogglePause() {
     if (videoPlayerController.value.isPlaying) {
       videoPlayerController.pause();
+      animationController.reverse();
     } else {
       videoPlayerController.play();
+      animationController.forward();
     }
     setState(() {
       isPlaying = !isPlaying;
